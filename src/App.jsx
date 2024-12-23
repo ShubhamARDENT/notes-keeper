040d6f688a2fbe37719f8e8e7cd3b1fc3373ebce
Shubham - ardent <shubham.chavan@ardentisys.com>
2024-12-23 15:39:44 +0530

    home page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from "../src/components/home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TodoList from "./components/TodoList";
+import StickyNotes from "./components/StickyNotes";
+import Navbar from "./components/Navbar";
+import { Box } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Box sx={{ marginTop: "115px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todolistapp" element={<TodoList />} />
+          <Route path="/stickynotes" element={<StickyNotes />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/home/index.jsx
@@ -0,0 +1,38 @@
+import { Box, Typography, Button } from "@mui/material";
+import { Link } from "react-router";
+
+
+const Home = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          fontSize: "2rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          minHeight: "50vh",
+        }}
+      >
+        <Typography variant="p" sx={{}}>
+          Welcome to Notes keeper
+        </Typography>
+        <Box sx={{ marginTop: "20px" }}>
+          <Button sx={{ background: "purple", marginRight: "10px" }}>
+            <Link to="/todolistapp" style={{ color: "white" }}>
+              To do list
+            </Link>
+          </Button>
+          <Button sx={{ background: "purple" }}>
+            <Link to="/stickynotes" style={{ color: "white" }}>
+              Sticky notes
+            </Link>
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Home;
